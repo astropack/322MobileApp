b3b0f8043cbf9e24559648db58709bbc7cd809b0
astropack <Student@OP-NWS201A-006.adt.nwu.ac.za>
2017-10-09 21:31:18 +0200

    test2.java and SplashScreen
    test2.java for hashing (not working yet)
    SplashScreen is a loading screen (is not called yet)

--- MyApplication/app/src/main/java/a324/mobileapplication/MainActivity.java
+++ MyApplication/app/src/main/java/a324/mobileapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package a324.mobileapplication;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -10,18 +11,29 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button btnFileChoose;
     private Button btnFileHashing;
     private Button btnValidate;
     private Button btnCheckVersion;
+    private Button btnShowValidatedFiles;
 
     private TextView textfileName;
     private static final int READ_REQUEST_CODE = 42;
     //private FileChooser fileChooserObj;
-    Intent intent;
+    private Intent intent;
     MainActivity mainObj = this;
+    private Uri uri = null;
+
+    test2 t2 = new test2();     //test2.java is still in progress
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         btnValidate = (Button) findViewById(R.id.btnValidate);
         btnCheckVersion = (Button) findViewById(R.id.btnCheckVersion);
         textfileName = (TextView) findViewById(R.id.textViewFileName);
+        btnShowValidatedFiles = (Button) findViewById(R.id.btnShowValidatedFiles);
         enableButtonsClick();
     }
     //Activate buttons for clicking:
@@ -46,24 +59,27 @@ public class MainActivity extends AppCompatActivity {
                 showChooser();
             }
         }));
-        /*
+
+        //Hashing of the file by using test2 class:
         btnFileHashing.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-
+                Toast.makeText(MainActivity.this, "Hashing should happen now", Toast.LENGTH_SHORT).show();
+                //t2.convertPDF();
             }
         }));
-        */
-        /*
+
+
         btnValidate.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
+                //write a record for the "file validated" to a file on the device to reference all submitted files.
+                writeSubmittedFiles();
+                SplashScreen sc = new SplashScreen();
             }
         }));
-        */
+
         /*
         btnCheckVersion.setOnClickListener((new View.OnClickListener() {
             @Override
@@ -73,7 +89,58 @@ public class MainActivity extends AppCompatActivity {
             }
         }));
         */
+
+        btnShowValidatedFiles.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                readSubmittedFiles();
+            }
+        }));
     }
+
+    private void writeSubmittedFiles() {
+        if (uri != null)
+        {
+            String uriStr = "" + uri;
+            String fileName = "submitted_files";
+
+            FileOutputStream fos;
+            try {
+                fos = openFileOutput(fileName, Context.MODE_APPEND);
+                fos.write((uriStr + "\n").getBytes());  //currently only writes the URIs to a file
+                fos.close();
+                Toast.makeText(MainActivity.this, "Saving uri to file", Toast.LENGTH_SHORT).show();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            uri = null;
+        }
+    }
+
+    private void readSubmittedFiles()
+    {
+        try {
+            String message = "";
+            FileInputStream fis = openFileInput("submitted_files");
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader bfr = new BufferedReader(isr);
+            StringBuffer strBuffer = new StringBuffer();
+            while ((message = bfr.readLine()) != null)
+            {
+                strBuffer.append(message + "\n");
+            }
+            Toast.makeText(MainActivity.this, strBuffer.toString(), Toast.LENGTH_SHORT).show();
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     //Open a file explorer to select a file:
     private void showChooser() {
 
@@ -89,7 +156,7 @@ public class MainActivity extends AppCompatActivity {
     public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
         if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
 
-            Uri uri = null;
+            //uri = null;
             if (resultData != null) {
                 uri = resultData.getData();
                 textfileName.setText("URI: " + uri);
